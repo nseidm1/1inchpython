ae06283d5160a82eabbb73a40bf376391263ac50
Noah Seidman <noah@noahseidman.com>
2020-06-09 10:43:29 -0400

    Instructions tweak.

--- OneInch.py
+++ OneInch.py
@@ -328,7 +328,7 @@ if __name__ == '__main__':
                        "\n[PRINT] print current pub address\n[APPROVE] approve token for swap (format: approve <token> <quantity>)\n[QUOTE] request a quote using the 1inch contract (format: quote <from_token> <to_token> <quantity>)"
                        "\n[BALANCE] print current Eth balance (format: balance)"
                        "\n[TOKENBALANCE] print current token balance (format: tokenbalance <token>)\n[API] probe 1inch for an arbitrage opportunity (format: api <from_token> <to_token> <quantity>)"
-                       "\n[ALLOWANCE] check allowance (format: allowance <token>)\n[TOKEN] print token info (format: token <token>)\n").upper()
+                       "\n[ALLOWANCE] check allowance for a specific token (format: allowance <token>)\n[TOKEN] print token info (format: token <token>)\n").upper()
         if action == 'LIST':
             oneInch.list_tokens()
         elif action == 'LOAD':
